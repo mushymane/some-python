2635cc27893b9176eeef977770bda3b1c92ce3fa
Luigi Lantin <mushymanee@gmail.com>
2020-12-19 14:19:04 -0800

    base code

--- image-playground/JPGtoPNGconverter.py
+++ image-playground/JPGtoPNGconverter.py
@@ -3,12 +3,20 @@ import os
 from PIL import Image
 
 # grab the first and second argument
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
 
 
 # check if new\ exists, if not create it
-
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
 # loop through pets, convert to PNG
-
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    clean_name = os.path.splitext(filename)[0]
+    print(clean_name)
+    img.save(f'{output_folder}{clean_name}.png', 'png')
+    print('done')
 
 # save to new folder
