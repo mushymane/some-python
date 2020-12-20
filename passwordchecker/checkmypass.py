26b7598c5a4cb0ae2aa5a75f05c465d052dee543
Luigi Lantin <mushymanee@gmail.com>
2020-12-19 17:48:48 -0800

    finished

--- passwordchecker/checkmypass.py
+++ passwordchecker/checkmypass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 def request_api_data(query_char):
@@ -11,18 +12,33 @@ def request_api_data(query_char):
     return res
 
 
-def read_res(response):
-    print(response.text)
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
 
 # check pw if it exists in API response
-
-
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first5chars, tail = sha1password[:5], sha1password[5:]
     response = request_api_data(first5chars)
-    print(response)
-    return read_res(response)
+    # print(response)
+    return get_password_leaks_count(response, tail)
 
 
-pwned_api_check('123')
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(
+                f'{password} was found {count} times... You should probably change your password')
+        else:
+            print(f'{password} was NOT found. Carry on then!')
+    return 'Stay safe!'
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
