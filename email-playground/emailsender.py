a11fc94fa4962b3c551426a7d60beb93a6cfe29a
Luigi Lantin <mushymanee@gmail.com>
2020-12-19 16:39:21 -0800

    changes

--- email-playground/emailsender.py
+++ email-playground/emailsender.py
@@ -1,17 +1,20 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Luigi Lantin'
 email['to'] = 'babypython1@gmail.com'
 email['subject'] = 'You got no jams'
 
-email.set_content('Jimin has funk and soul')
+email.set_content(html.substitute({'name': 'Namjoon'}), 'html')
 
 # sets host as gmail
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('babypython1@gmail.com', 'hahahehehoho')
+    smtp.login('babypython1@gmail.com', '')
     smtp.send_message(email)
     print('all good boss mane')
