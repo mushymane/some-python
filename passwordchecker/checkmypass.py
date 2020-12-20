0330353894fdb3fc7f9d008537ab1517d30b3449
Luigi Lantin <mushymanee@gmail.com>
2020-12-19 17:26:48 -0800

    adding more stuff

--- passwordchecker/checkmypass.py
+++ passwordchecker/checkmypass.py
@@ -8,13 +8,21 @@ def request_api_data(query_char):
     if res.status_code != 200:
         raise RuntimeError(
             f'Error fetching: {res.status_code}, check the api and try again')
-        return res
+    return res
 
+
+def read_res(response):
+    print(response.text)
 
 # check pw if it exists in API response
+
+
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    return sha1password
+    first5chars, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5chars)
+    print(response)
+    return read_res(response)
 
 
 pwned_api_check('123')
